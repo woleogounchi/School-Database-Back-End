ab522779fbd8a20252b1479dc3130478fe6d20e6
Kolawole Ogounchi <kolawoleogounchi@outlook.com>
2019-11-29 08:18:06 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ const auth = require('basic-auth');
 const db = require('./db');
 const { Course, User } = db.models;
 
+// Construct a router instance.
+const router = express.Router();
+
 // Handler function to wrap each route.
 function asyncHandler(cb){
   return async (req,res, next) => {
@@ -24,68 +27,115 @@ function asyncHandler(cb){
 // as they are created.
 const users = [];
 
-// // Middleware to authenticate the request using Basic Authentication.
-// const authenticateUser = (req, res, next) => {
-//   let message = null;
-//   // Get the user's credentials from the Authorization header.
-//   const credentials = auth(req);
+// Middleware to authenticate the request using Basic Authentication.
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+  // Get the user's credentials from the Authorization header.
+  const credentials = auth(req);
+  // If user credentials exist
+  if (credentials) {
+    // Look for a user whose `emailAddress` matches the credentials `name` property.
+    const user = await User.findOne({
+      where: { emailAddress: credentials.name}
+    });
+    // if user retrieved then use bcryptjs to compare password provided in Auth Header to the one stored in db 
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      // if password matches i.e. user is authenticated
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+  // In case Auth is a failure
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
 
-//   if (credentials) {
-//     // Look for a user whose `username` matches the credentials `name` property.
-//     const user = users.find(u => u.username === credentials.name);
+// GET /api/users 200 - Returns the currently authenticated user
+router.get('/users', asyncHandler(async (req, res) => {
+  try {
+    const user = req.body;
+    if (user.password) {
+      user.password = bcryptjs.hashSync(user.password);
+    }
+    if (!user.emailAddress) {
+      user.emailAddress = "";
+    }
+    await User.findOrCreate({ where: { emailAddress: user.emailAddress }, defaults: user })
+      .then(([user, created]) => {
+        if (created) {
+          console.log("New user successfully created");
+          res.status(201).set("Location", "/").end();
+        } else {
+          console.log(`There is already an existing account with the following email address: ${user.emailAddress}`);
+          res.status(200).set("Location", "/").end();
+        }
+      });
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const errorMessage = error.errors.map(error => error.message);
+      res.status(400).json({ errors: errorMessage });
+    } else {
+        throw error;
+    }
+  }
+}));
 
-//     if (user) {
-//       const authenticated = bcryptjs
-//         .compareSync(credentials.pass, user.password);
-//       if (authenticated) {
-//         console.log(`Authentication successful for username: ${user.username}`);
+/* Create the course routes */
 
-//         // Store the user on the Request object.
-//         req.currentUser = user;
-//       } else {
-//         message = `Authentication failure for username: ${user.username}`;
-//       }
-//     } else {
-//       message = `User not found for username: ${credentials.name}`;
-//     }
-//   } else {
-//     message = 'Auth header not found';
-//   }
+// GET /api/courses 200 - Returns a list of courses
+router.get('/courses', asyncHandler(async(req, res, next) => {
+    const courses = await Course.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ['emailAddress','password','createdAt', 'updatedAt']
+          }
+        }
+      ]
+    });
+    res.status(200).json(courses);
+}));
 
-//   if (message) {
-//     console.warn(message);
-//     res.status(401).json({ message: 'Access Denied' });
-//   } else {
-//     next();
-//   }
-// };
-
-// Construct a router instance.
-const router = express.Router();
-
-// Route that returns the current authenticated user.
-router.get('/users', (req, res) => {
-  const user = req.currentUser;
-
-  res.json({
-    firstName: users.firstName,
-    laststName: users.lastName,
-  });
-
-  // Set the status to 200 and end the response.
-  return res.status(200).end();
-});
-
-// Route that creates a new user.
-router.post('/users', (req, res) => {
-  // Get the user from the request body.
-  const user = req.body;
-
-  // Add the user to the `users` array.
-  users.push(user);
-
-  // Set the status to 201 Created and end the response.
-  res.status(201).set('Location', '/').end();
-});
+// GET /api/courses/:id 200 - Returns a the course
+router.get('/courses/:id', asyncHandler(async(req, res) => {
+    const course = await Course.findByPk(req.params.id,
+      {
+        attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ['emailAddress','password','createdAt', 'updatedAt']
+          }
+        }
+      ]
+    });
+    if (course) {
+      res.status(200).json(course);
+    } else {
+      res.status(400).withMessage('Sorry course not found');
+    }
+}));
 
 module.exports = router;
